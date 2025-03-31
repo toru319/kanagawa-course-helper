cc626092d760d66660f7f0a25bee0204b5495e6f
toru319 <toru319319@icloud.com>
2025-03-31 12:18:01 +0900

    new

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint';
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/kanagawa-course-helper/',
+  plugins: [
+    react(),
+    eslint({
+      cache: false,
+      include: ['./src/**/*.js', './src/**/*.jsx'],
+      exclude: [],
+    }),
+  ],
+  build: {
+    outDir: 'build', // ビルド出力ディレクトリ
+  },
+})
